0f7d68f891f8b28b611c64cee8a3647e9bb6b013
HerryZeng <herry.zeng@gmail.com>
2020-11-24 23:14:13 +0800

    2020112402

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,76 +1,59 @@
 <template>
-  <a-form
-    ref="ruleForm"
-    :model="form"
-    :rules="rules"
-    :label-col="labelCol"
-    :wrapper-col="wrapperCol"
-  >
-    <a-form-item ref="name" label="用户名" name="name">
+  <a-form :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+    <a-form-item label="用户名">
       <a-input v-model:value="form.name" />
     </a-form-item>
-    <a-form-item ref="password" label="密码" name="password">
-      <a-input v-model:value="form.password" type="password" />
+
+    <a-form-item label="密码">
+      <a-input v-model:value="form.password" type="password"/>
     </a-form-item>
-    <a-form-item ref="fileupload" label="文件上传" name="fileupload">
-      <a-input v-model:value="form.password" type="file" />
+
+
+    <a-form-item label="上传文件">
+      <a-input type="file"/>
     </a-form-item>
+
+
     <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
-      <a-button type="primary" @click="onSubmit"> 提交 </a-button>
-      <a-button style="margin-left: 10px" @click="resetForm"> 清空 </a-button>
+      <a-button type="primary" @click="onSubmit">
+        提交
+      </a-button>
+      <a-button style="margin-left: 10px;">
+        返回
+      </a-button>
     </a-form-item>
+    
   </a-form>
 </template>
 <script>
-import qs from "qs";
+
+// import qs from "qs"
+
 export default {
+
   data() {
     return {
       labelCol: { span: 4 },
       wrapperCol: { span: 14 },
-      other: "",
       form: {
-        name: "",
-        password: "",
-      },
-      rules: {
-        name: [
-          { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 3, max: 15, message: "3-5字符之间", trigger: "blur" },
-        ],
-        password: [
-          { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 8, max: 20, message: "8-20字符之间", trigger: "blur" },
-        ],
+        name: '',
+        password: '',
       },
     };
   },
   methods: {
     onSubmit() {
-      this.$refs.ruleForm
-        .validate()
-        .then(() => {
-          console.log("values", this.form);
-          console.log(this.form.name);
-          console.log(this.form.password);
-          this.$axios
-            .post("/ch11/post_test", qs.stringify({
-              name: this.form.name,
-              password: this.form.password,
-            }))
-            .then((res) => {
-              console.log(res);
-            })
-            .catch((res) => {
-              console.log(res);
+
+        const formData = new FormData();
+        formData.append("name",this.form.name);
+        formData.append("password",this.form.password);
+        formData.append("upload_file",document.querySelector('input[type=file]').files[0])
+     
+        this.$axios.post('/ch11/upload_file',formData).then(function (response) {
+                    console.log(response);
+                }).catch(function (error) {
+                    console.log(error);
             });
-        })
-        .catch((error) => {
-          console.log("error", error);
-        });
-    },
-    resetForm() {
-      this.$refs.ruleForm.resetFields();
     },
   },
 };
--- src/components/FileUploads.vue
+++ src/components/FileUploads.vue
@@ -1,73 +1,66 @@
 <template>
-  <a-form
-    ref="ruleForm"
-    :model="form"
-    :rules="rules"
-    :label-col="labelCol"
-    :wrapper-col="wrapperCol"
-  >
-    <a-form-item ref="name" label="用户名" name="name">
+  <a-form :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+    <a-form-item label="用户名">
       <a-input v-model:value="form.name" />
     </a-form-item>
-    <a-form-item ref="password" label="密码" name="password">
+
+    <a-form-item label="密码">
       <a-input v-model:value="form.password" type="password" />
     </a-form-item>
+
+    <a-form-item label="上传文件">
+      <a-input type="file" class="files" />
+    </a-form-item>
+
+    <a-form-item label="上传文件">
+      <a-input type="file" class="files" />
+    </a-form-item>
+
+    <a-form-item label="上传文件">
+      <a-input type="file" class="files" />
+    </a-form-item>
+
     <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
       <a-button type="primary" @click="onSubmit"> 提交 </a-button>
-      <a-button style="margin-left: 10px" @click="resetForm"> 清空 </a-button>
+      <a-button style="margin-left: 10px"> 返回 </a-button>
     </a-form-item>
   </a-form>
 </template>
 <script>
-import qs from "qs";
+// import qs from "qs"
+
 export default {
   data() {
     return {
       labelCol: { span: 4 },
       wrapperCol: { span: 14 },
-      other: "",
       form: {
         name: "",
         password: "",
-      },
-      rules: {
-        name: [
-          { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 3, max: 15, message: "3-5字符之间", trigger: "blur" },
-        ],
-        password: [
-          { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 8, max: 20, message: "8-20字符之间", trigger: "blur" },
-        ],
       },
     };
   },
   methods: {
     onSubmit() {
-      this.$refs.ruleForm
-        .validate()
-        .then(() => {
-          console.log("values", this.form);
-          console.log(this.form.name);
-          console.log(this.form.password);
-          this.$axios
-            .post("/ch11/post_test", qs.stringify({
-              name: this.form.name,
-              password: this.form.password,
-            }))
-            .then((res) => {
-              console.log(res);
-            })
-            .catch((res) => {
-              console.log(res);
-            });
+      const formData = new FormData();
+      formData.append("name", this.form.name);
+      formData.append("password", this.form.password);
+      const files = document.getElementsByClassName("files");
+      // console.log(document.getElementsByClassName("files")[0].files[0].name);
+      for (let i = 0; i < files.length; i++) {
+        console.log(files[i].files[0]);
+        console.log("before append");
+        formData.append("upload_files", files[i].files[0]);
+         console.log("after append");
+      }
+      this.$axios
+        .post("/ch11/upload_files", formData)
+        .then(function (response) {
+          console.log(response);
         })
-        .catch((error) => {
-          console.log("error", error);
+        .catch(function (error) {
+          console.log(error);
         });
-    },
-    resetForm() {
-      this.$refs.ruleForm.resetFields();
     },
   },
 };
